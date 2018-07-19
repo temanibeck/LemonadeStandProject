a0ab925c4a1ef6a236059b68ac49570064ea348b
Temani Beck <temanibeck@gmail.com>
2018-07-19 10:28:21 -0500

    Changed double values to decimal values and made minor bug/huge exception changes

--- LemonadeStandProject/Program.cs
+++ LemonadeStandProject/Program.cs
@@ -8,9 +8,8 @@ namespace LemonadeStandProject
         {
             LemonadeStand lemonadeStand = new LemonadeStand();
 
-            Console.WriteLine("Hello there! Thank you for expressing interest " +
-                              "in opening your very own lemonade stand. What" +
-                              "would you like to call your lemonade stand?");
+            Console.WriteLine("Hello there! Thank you for expressing interest in opening your very own lemonade stand. What would " +
+                              "you like to call your lemonade stand?");
 
             lemonadeStand.Name = Console.ReadLine();
 
@@ -18,25 +17,25 @@ namespace LemonadeStandProject
 
             Console.WriteLine("Now let's get down to the numbers. How many cups do you intend to sell?");
 
-            lemonadeStand.NumberOfCups = int.Parse(Console.ReadLine());
+            lemonadeStand.NumberOfCups = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine(lemonadeStand.NumberOfCups + " is a good number to start with especially since this is a new stand.");
 
             Console.WriteLine("Now how much do you intend to sell each cup for?");
 
-            lemonadeStand.PricePerCup = double.Parse(Console.ReadLine());
+            lemonadeStand.PricePerCup = decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("Customers paying $" + lemonadeStand.PricePerCup + " for each cup of lemonade is a perfect price point.");
 
             Console.WriteLine("Now tell me how much you intend to spend on ingredients needed to make each cup?");
 
-           lemonadeStand.CostPerCup = double.Parse(Console.ReadLine());
+            lemonadeStand.CostPerCup = decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("I can tell that you have given some thought to the amount you want to spend per cup");
 
-            double netProfitPerCup = lemonadeStand.PricePerCup - lemonadeStand.CostPerCup;
+            decimal netProfitPerCup = lemonadeStand.PricePerCup - lemonadeStand.CostPerCup;
 
-            double netProfitTotal = netProfitPerCup * lemonadeStand.NumberOfCups;
+            decimal netProfitTotal = netProfitPerCup * lemonadeStand.NumberOfCups;
 
             Console.WriteLine("After taking a closer look at your numbers you can anticipate earning $" + netProfitTotal + ".");
         }
