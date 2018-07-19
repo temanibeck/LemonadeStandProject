f4b131a748e795fdd3c887bf076b4c262a37e46d
Temani Beck <temanibeck@gmail.com>
2018-07-19 10:13:46 -0500

    Added number of cups question and response and added lemonade stand class

--- LemonadeStandProject/Program.cs
+++ LemonadeStandProject/Program.cs
@@ -15,6 +15,12 @@ namespace LemonadeStandProject
             lemonadeStand.Name = Console.ReadLine();
 
             Console.WriteLine("I think that's a lovely name! So you will be opening a lemonade stand called " + lemonadeStand.Name + ".");
+
+            Console.WriteLine("Now let's get down to the numbers. How many cups do you intend to sell?");
+
+            lemonadeStand.NumberOfCups = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(lemonadeStand.NumberOfCups + " is a good number to start with especially since this is a new stand.");
         }
     }
 }
