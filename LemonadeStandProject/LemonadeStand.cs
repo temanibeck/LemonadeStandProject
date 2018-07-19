c5762da52e9267c55581b31f4e45ac0e884cae1f
Temani Beck <temanibeck@gmail.com>
2018-07-19 10:11:22 -0500

    Added welcome and name request

---
+++ LemonadeStandProject/LemonadeStand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LemonadeStandProject
+{
+    public class LemonadeStand
+    {
+        public string Name { get; set; }
+        public int NumberOfCups { get; set; }
+        public decimal PricePerCup { get; set; }
+        public decimal CostPerCup { get; set; }
+    }
+}
--- LemonadeStandProject/Program.cs
+++ LemonadeStandProject/Program.cs
@@ -6,7 +6,15 @@ namespace LemonadeStandProject
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LemonadeStand lemonadeStand = new LemonadeStand();
+
+            Console.WriteLine("Hello there! Thank you for expressing interest " +
+                              "in opening your very own lemonade stand. What" +
+                              "would you like to call your lemonade stand?");
+
+            lemonadeStand.Name = Console.ReadLine();
+
+            Console.WriteLine("I think that's a lovely name! So you will be opening a lemonade stand called " + lemonadeStand.Name + ".");
         }
     }
 }
